e085f7bd69b9bab0366e1d60db757d4e67bce887
YanFrenklakh <yanfr2@ac.sce.ac.il>
2022-12-08 05:36:24 +0200

    Base template & costume login logic

--- YosefZal/accounts/urls.py
+++ YosefZal/accounts/urls.py
@@ -2,8 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('dashboard/', views.dashboard),
+    # Authentication urls
+    path('register/', views.register_page, name="register"),
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+
+    # home page urls
+    path('', views.home, name="home"),
+    path('home/', views.home, name="home"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+
+    # User urls
+    path('users/', views.users, name="users"),
+    path('users/Doctor/', views.user_doctor, name="doctor"),
+    path('users/Patient/', views.user_patient, name="patient"),
+    path('users/Administrator/', views.user_admin, name="administrator"),
+
 
 ]
 
